69a8030f491e9cf05fdb44f6630487a20a704a17
korigamik <korigamik@gmail.com>
2022-09-14 14:13:03 +0530

    add experiments and snippets

---
+++ Microprocessor and Microcontrollers/code/fib.asm
@@ -0,0 +1,23 @@
+;Declaration Part
+.MODEL SMALL
+.DATA
+RES DB ?
+CNT DB 0AH       ; Initialize the counter for the number of Fibonacci numbers needed
+.CODE
+START: MOV AX, @DATA
+MOV DS,AX
+LEA SI,RES
+MOV CL,CNT       ; Load the count value for CL for looping
+MOV AX,00H       ; Default No
+MOV BX,01H       ; Default No
+
+;Fibonacci Part
+L1:ADD AX,BX
+MOV [SI],AX
+MOV AX,BX
+MOV BX,[SI]
+INC SI
+LOOP L1
+
+INT 3H           ; Terminate the Program
+END START
