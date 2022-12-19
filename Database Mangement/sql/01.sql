30dd185baa544a67947ef6ad71a6dc31cc195683
korigamik <korigamik@gmail.com>
2022-12-19 23:55:39 +0530

    question 1 done

--- Database Mangement/sql/01.sql
+++ Database Mangement/sql/01.sql
@@ -1,3 +1,6 @@
-SELECT E.EmpFname, E.EmpLname, P.EmpPosition 
-FROM EmployeeInfo E INNER JOIN EmployeePosition P ON
-E.EmpID = P.EmpID AND P.EmpPosition IN ('Manager');
+SELECT sname 
+FROM SAILORS 
+WHERE sid IN (
+    SELECT sid 
+    FROM RESERVES
+);
